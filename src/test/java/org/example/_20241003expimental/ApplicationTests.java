40bde2393d867dce76c41b5bf9978cd68a3911c5
alfo0924 <halo0924@yahoo.com.tw>
2024-10-03 20:51:09 +0800

    v1.01 init cnp

---
+++ src/test/java/org/example/_20241003expimental/ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example._20241003expimental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
